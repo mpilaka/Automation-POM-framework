965420c5a4c095a2cb23b37aee741136c42fdfe6
mpilaka <47941426+mpilaka@users.noreply.github.com>
2025-05-08 10:10:48 -0400

    Create Environment.java

---
+++ src/main/java/com/automation/constants/Environment.java
@@ -0,0 +1,12 @@
+/**
+ * File: src/main/java/com/cms/constants/Environment.java
+ * Description: Enum for different environments
+ */
+package com.cms.constants;
+
+public enum Environment {
+    DEV,
+    QA,
+    STAGE,
+    PROD
+}
