640878bf5204c22ee8b7a8fd1bfbacef1cac0daa
mpilaka <47941426+mpilaka@users.noreply.github.com>
2025-05-08 10:10:09 -0400

    Create BrowserType.java

---
+++ src/main/java/com/automation/constants/BrowserType.java
@@ -0,0 +1,13 @@
+
+/**
+ * File: src/main/java/com/cms/constants/BrowserType.java
+ * Description: Enum for supported browser types
+ */
+package com.cms.constants;
+
+public enum BrowserType {
+    CHROME,
+    FIREFOX,
+    EDGE,
+    SAFARI
+}
